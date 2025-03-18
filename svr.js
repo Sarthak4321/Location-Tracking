414fb9d90a595f769b88360742be7704bea7dd47
Sarthak4321 <roysarthak16@gmail.com>
2025-03-18 10:56:02 +0530

    adding

--- svr.js
+++ svr.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const socketio = require("socket.io");
+const path = require("path");
+
 
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engien", "ejs");
-app.set
+// app.set(express.static(path.join(__dirname, "public")));
+
+
+// io.on("connection", (socket) => {
+//     console.log("a user connected");
+// })
+
 app.get("/", (req, res) => {
-    res.send("i'm hare");
-    console.log("listening...");
+    res.render("index");
+    
 });
 
 app.listen(3000);
