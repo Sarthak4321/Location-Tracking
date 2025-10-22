c8b99da29731049bef11fac883f505e5704b5ef2
Sarthak4321 <roysarthak16@gmail.com>
2025-10-22 22:17:12 +0530

    Fix Vercel routing

--- svr.js
+++ svr.js
@@ -4,28 +4,27 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 
-
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.use('/public', express.static(path.join(__dirname, 'public')));
-// app.set(express.static(path.join(__dirname, "public")));
-
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'views'));
 
 io.on("connection", (socket) => {
     socket.on("sending-location", (data) => {
-        io.emit("spreading-location", {id: socket.id, ...data});
-    })
+        io.emit("spreading-location", { id: socket.id, ...data });
+    });
 
     socket.on("user-disconnected", () => {
         io.emit("user-disconnected", socket.id);
-    })
+    });
 });
 
 app.get("/", (req, res) => {
     res.render("index");
-    
 });
 
-server.listen(3000);
+// ⚠️ Do NOT call server.listen()
+// Just export it
+module.exports = server;
